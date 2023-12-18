c4581d9cc355dd028d45c885a07b1ca6474ea06f
rafaelaltamirano <rafaelaltamirano_@outlook.com>
2023-12-18 15:03:46 +0100

    adding shared preferences setup and DI

---
+++ app/src/main/java/com/plcoding/calorytracker/di/AppModule.kt
@@ -0,0 +1,24 @@
+package com.plcoding.calorytracker.di
+
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import com.example.core.domain.preferences.DefaultPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(app: Application): SharedPreferences = app.getSharedPreferences("shared_pref",MODE_PRIVATE)
+    @Provides
+    @Singleton
+    fun providePreferences(sharedPreferences: SharedPreferences) = DefaultPreferences(sharedPreferences)
+
+}
---
+++ core/src/main/java/com/example/core/domain/preferences/DefaultPreferences.kt
@@ -0,0 +1,99 @@
+package com.example.core.domain.preferences
+
+import android.content.SharedPreferences
+import com.example.core.domain.model.ActivityLevel
+import com.example.core.domain.model.Gender
+import com.example.core.domain.model.GoalType
+import com.example.core.domain.model.UserInfo
+import com.example.core.domain.preferences.Preferences.Companion.KEY_ACTIVITY_LEVEL
+import com.example.core.domain.preferences.Preferences.Companion.KEY_AGE
+import com.example.core.domain.preferences.Preferences.Companion.KEY_CARB_RATIO
+import com.example.core.domain.preferences.Preferences.Companion.KEY_FAT_RATIO
+import com.example.core.domain.preferences.Preferences.Companion.KEY_GENDER
+import com.example.core.domain.preferences.Preferences.Companion.KEY_GOAL_TYPE
+import com.example.core.domain.preferences.Preferences.Companion.KEY_HEIGHT
+import com.example.core.domain.preferences.Preferences.Companion.KEY_PROTEIN_RATIO
+import com.example.core.domain.preferences.Preferences.Companion.KEY_WEIGHT
+
+class DefaultPreferences
+    (
+    private val sharedPreferences: SharedPreferences
+) : Preferences {
+    override fun saveGender(gender: Gender) {
+       sharedPreferences.edit()
+            .putString(KEY_GENDER, gender.name)
+            .apply()
+    }
+
+    override fun saveAge(age: Int) {
+        sharedPreferences.edit()
+            .putInt(KEY_AGE, age)
+            .apply()
+    }
+
+    override fun saveWeight(weight: Float) {
+        sharedPreferences.edit()
+            .putFloat(KEY_WEIGHT, weight)
+            .apply()
+    }
+
+    override fun saveHeight(height: Int) {
+        sharedPreferences.edit()
+            .putInt(KEY_HEIGHT, height)
+            .apply()
+    }
+
+    override fun saveActivityLevel(level: ActivityLevel) {
+        sharedPreferences.edit()
+            .putString(KEY_ACTIVITY_LEVEL, level.name)
+            .apply()
+    }
+
+    override fun saveGoalType(type: GoalType) {
+        sharedPreferences.edit()
+            .putString(KEY_GOAL_TYPE, type.name)
+            .apply()
+    }
+
+    override fun saveCarbRatio(ratio: Float) {
+        sharedPreferences.edit()
+            .putFloat(KEY_CARB_RATIO, ratio)
+            .apply()
+    }
+
+    override fun saveProteinRatio(ratio: Float) {
+        sharedPreferences.edit()
+            .putFloat(KEY_PROTEIN_RATIO, ratio)
+            .apply()
+    }
+
+    override fun saveFatRatio(ratio: Float) {
+        sharedPreferences.edit()
+            .putFloat(KEY_FAT_RATIO, ratio)
+            .apply()
+    }
+
+    override fun loadUserInfo(): UserInfo {
+        val age = sharedPreferences.getInt(KEY_AGE, 20)
+        val gender = sharedPreferences.getString(KEY_GENDER, Gender.Male.name)
+        val height = sharedPreferences.getInt(KEY_HEIGHT, 160)
+        val weight = sharedPreferences.getFloat(KEY_WEIGHT, 80f)
+        val activityLevel = sharedPreferences.getString(KEY_ACTIVITY_LEVEL, ActivityLevel.Medium.name)
+        val goalType = sharedPreferences.getString(KEY_GOAL_TYPE, GoalType.KeepWeight.name)
+        val carbRatio = sharedPreferences.getFloat(KEY_CARB_RATIO, 0f)
+        val proteinRatio = sharedPreferences.getFloat(KEY_PROTEIN_RATIO, 0f)
+        val fatRatio = sharedPreferences.getFloat(KEY_FAT_RATIO, 0f)
+        return UserInfo(
+
+            gender = Gender.fromString(gender ?: "male"),
+            age = age,
+            weight = weight,
+            height = height,
+            activityLevel = ActivityLevel.fromString(activityLevel ?: "medium"),
+            goalType = GoalType.fromString(goalType ?: "keep_weight"),
+            carbRatio = carbRatio,
+            proteinRatio = proteinRatio,
+            fatRatio = fatRatio
+        )
+    }
+}
