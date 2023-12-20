67b5fd69869490e98a8d9b05a5155320d2abfb0e
rafaelaltamirano <rafaelaltamirano_@outlook.com>
2023-12-20 15:01:48 +0100

    add first components of gender screen

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
     implementation("com.android.tools.build:gradle:8.0.1")
-    implementation("com.squareup:javapoet:1.13.0") // <-- added this
+    implementation("com.squareup:javapoet:1.13.0")
 }
 
 
